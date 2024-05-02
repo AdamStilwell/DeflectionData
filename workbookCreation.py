f1e842a7c541d0c79b0e317aaf75ad60db72bba4
Stilwell <adam.stilwell@dupont.com>
2024-05-02 10:03:21 -0400

    Made the code more functional. Made the headers more readable.

--- workbookCreation.py
+++ workbookCreation.py
@@ -7,9 +7,9 @@ class Workbook:
         worksheet = self.workbook.add_worksheet(title)
         return worksheet
 
-    def create_chart(self, worksheet):
+    def create_chart(self, worksheet, location):
         chart = self.workbook.add_chart({"type": "scatter", "subtype": "smooth"})
-        worksheet.insert_chart("A1", chart,
+        worksheet.insert_chart(location, chart,
                                {"x_scale": 1.5, "y_scale": 1.5})
         return chart
 
@@ -22,14 +22,19 @@ class Workbook:
                          "G1c (J/m2)", "Peak Detach Pressure (MPa)", "Maximum % Deflection",
                          "Minimum Gap (mm)", "Distance to Break (um)", "Amplitude",
                          "Power Law Index", "Offset", "FTA-4 equiv.", "G'20 C", "G' 200 C"]
-        self.worksheet_summary.write_row("A1", headers_array)
+        cell_format_string = self.workbook.add_format({"bold": True})
+        cell_format_string.set_align("right")
+        self.worksheet_summary.write_row("A1", headers_array, cell_format_string)
         self.worksheet_summary.autofit()
 
         self.worksheet_pressure_deflection = self.create_worksheet("Pressure Deflection 450N")
-        self.pressure_deflection_chart = self.create_chart(worksheet=self.worksheet_pressure_deflection)
+        self.pressure_deflection_chart = self.create_chart(worksheet=self.worksheet_pressure_deflection,
+                                                           location="A1")
 
         self.worksheet_force_displacement = self.create_worksheet("Force-Displacement")
-        self.force_displacement_chart = self.create_chart(worksheet=self.worksheet_force_displacement)
+        self.force_displacement_chart = self.create_chart(worksheet=self.worksheet_force_displacement,
+                                                          location="A1")
 
         self.worksheet_pressure_compression_rate = self.create_worksheet("Pressure-Compression Rate")
-        self.pressure_compression_rate_chart = self.create_chart(worksheet=self.worksheet_pressure_compression_rate)
+        self.pressure_compression_rate_chart = self.create_chart(worksheet=self.worksheet_pressure_compression_rate,
+                                                                 location="A1")
